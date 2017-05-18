63b901ab7958b8b7624f2c2114663db989470258
timor <timor.dd@googlemail.com>
2017-05-18 14:35:56 +0200

    remove old workaround

--- default.nix
+++ default.nix
@@ -8,29 +8,6 @@ in
 
 {
 
-  # fetchFromGitHub = with (inherit self fetchgit); {
-  #   owner, repo, rev, name ? "${repo}-${rev}-src", leaveDotGit ? false,
-  #   fetchSubmodules ? false,
-  #   ... # For hash agility
-  # }@args:
-  # let
-  #   baseUrl = "https://github.com/${owner}/${repo}";
-  #   passthruAttrs = removeAttrs args [ "owner" "repo" "rev" "fetchSubmodules" ];
-  # in if (fetchSubmodules then
-  #   fetchgit ({
-  #     inherit name rev fetchSubmodules;
-  #     url = "${baseUrl}.git";
-  #   } // passthruAttrs)
-  # else
-  #   # We prefer fetchzip in cases we don't need submodules as the hash
-  #   # is more stable in that case.
-  #   fetchzip ({
-  #     inherit name;
-  #     url = "${baseUrl}/archive/${rev}.tar.gz";
-  #     meta.homepage = "${baseUrl}/";
-  #   } // passthruAttrs) // { inherit rev; };
-
-
   factor-lang = callPackage ./pkgs/factor-lang {
     inherit (self.gnome2) gtkglext;
   };
