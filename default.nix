738dbb1db354d29c74619e1190f9210acc89d436
timor <timor.dd@googlemail.com>
2017-09-16 00:25:44 +0200

    remove outdated alsaTools change

--- default.nix
+++ default.nix
@@ -20,19 +20,6 @@ in
       buildInputs = oldAttrs.buildInputs ++ [ self.spnav ];
   }));
 
-  alsaTools = (super.alsaTools.overrideAttrs (oldAttrs: rec {
-      name = "alsa-tools-${version}";
-      version = "1.1.3";
-
-      src = self.fetchurl {
-        urls = [
-          "ftp://ftp.alsa-project.org/pub/tools/${name}.tar.bz2"
-          "http://alsa.cybermirror.org/tools/${name}.tar.bz2"
-        ];
-      sha256 = "02b75fyfmm9m2iz59d3xa97cas4f697a4pzdxn1i64kjd36iv3yq";
-      };
-  }));
-
   networkmanagerOC = super.networkmanager.overrideAttrs (oldAttrs: {
     name = "network-manager-ocpatch-${oldAttrs.version}";
     patches = oldAttrs.patches ++ [ ./patches/openconnect_helper_path.patch ];
