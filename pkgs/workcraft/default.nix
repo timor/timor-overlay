8110ff023b88261a69c154b9151ca9d286e8ee70
timor <timor.dd@googlemail.com>
2018-03-26 10:26:20 +0200

    workcraft: mirror pull request

--- pkgs/workcraft/default.nix
+++ pkgs/workcraft/default.nix
@@ -1,23 +1,32 @@
-{ stdenv, pkgs, fetchurl, jre, gradle, minisat, makeWrapper, bash }:
+{ stdenv, pkgs, fetchurl, jre, makeWrapper }:
 
 stdenv.mkDerivation rec {
-  name = "workcraft";
+  name = "workcraft-${version}";
+  version = "3.1.9";
 
   src = fetchurl {
-    url = "https://github.com/workcraft/workcraft/releases/download/v3.1.3/workcraft-v3.1.3-linux.tar.gz";
-    sha256 = "1dl97jw55y2wil6vs40z28ayxkhyw4s7lzkjwda77c6vc6c4wsnc";
+    url = "https://github.com/workcraft/workcraft/releases/download/v${version}/workcraft-v${version}-linux.tar.gz";
+    sha256 = "0d1mi8jffwr7irp215j9rfpa3nmwxrx6mv13bh7vn0qf6i0aw0xi";
   };
 
-  buildInputs = [ bash makeWrapper ];
+  buildInputs = [ makeWrapper ];
 
   phases = [ "unpackPhase" "installPhase" "fixupPhase"];
-
 
   installPhase = ''
   mkdir -p $out/share
   cp -r * $out/share
   mkdir $out/bin
   makeWrapper $out/share/workcraft $out/bin/workcraft \
-    --set JAVA_HOME "${jre}";
+    --set JAVA_HOME "${jre}" \
+    --set _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on';
   '';
+
+  meta = {
+    homepage = http://workcraft.org/;
+    description = "Framework for interpreted graph modeling, verification and synthesis";
+    platforms = stdenv.lib.platforms.linux;
+    maintainers = with stdenv.lib.maintainers; [ timor ];
+    inherit version;
+  };
 }
