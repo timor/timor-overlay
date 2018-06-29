f5472c9634d89f4bfc6ab8ad3a9bf2fc401a61ef
timor <timor.dd@googlemail.com>
2018-06-29 17:07:02 +0200

    gdbForPackages: init
    This allows to conveniently build environments where gdb can be used to debug
    a set of binaries from the specified list of packages.  Note that the thing does
    depend on the generic builder being used, because until I learn how to correctly
    use setup hooks, the buildPhase attributes of each specified package
    are being (mis-)used to:
    
    1. Save a copy of the patched and configured source right before building
    2. Install this copy into the store

--- default.nix
+++ default.nix
@@ -3,10 +3,96 @@ self: super:
 # self: final fixpoint
 # super: immediate predecessor pkgs
 let
+  lib = self.lib;
   callPackage = super.lib.callPackageWith self;
+  debugify = let
+    saveSource = ''
+      savedSource="$(mktemp -d)"
+      echo debugify: saving source from $NIX_BUILD_TOP/$sourceRoot to $savedSource
+      cp -r $NIX_BUILD_TOP/$sourceRoot $savedSource/
+      '';
+    installSource = ''
+      echo debugify: moving saved source to store
+      if [ -z "$savedSource" ]; then
+        echo debugify: unable to find saved source; exit 1
+      fi
+      mkdir -p $source
+      echo mv $savedSource/* $source/
+      mv $savedSource/* $source/
+      '';
+  in
+    (pkg: pkg.overrideAttrs (oldAttrs:
+      {
+        name = oldAttrs.name +"-debug";
+        separateDebugInfo = true;
+        outputs = (oldAttrs.outputs or [ "out" ]) ++ [ "source" ];
+      } // (
+        if (oldAttrs ? buildPhase) then {
+          buildPhase = saveSource + oldAttrs.buildPhase;
+        } else {
+          preBuild = (oldAttrs.preBuild or "") + saveSource;
+        }) // (
+        if (oldAttrs ? installPhase) then {
+          installPhase = installSource + oldAttrs.installPhase;
+        } else {
+          preInstall = installSource + (oldAttrs.preInstall or "");
+        })));
 in
 
 {
+
+  # gdbForPackages: build a set of packages with debug information and source,
+  # wrap a gdb that knows about the respective source locations and debug
+  # symbols
+  #
+  # Some Examples:
+  # 1. Enter a shell where `gdb glxinfo` and `gdb hello` does what one expects:
+  # $ nix-shell -E 'with import <nixpkgs> {}; (gdbForPackages [glxinfo hello ]).env'
+  #
+  # 2. Install the set of relevant binaries for e.g. glxinfo and the corresponding gdb into user environment:
+  # $ nix-build -E 'with import <nixpkgs> {}; (gdbForPackage glxinfo)'
+  # $ nix-env -i ./result
+  gdbForPackages = (pkgs:
+    let
+    debugPkgs = map debugify pkgs;
+    debugFileStore = self.buildEnv {
+      name = "gdb-env-debug-files";
+      paths = debugPkgs;
+      extraOutputsToInstall = [ "debug" "source" ];
+    };
+    gdbInit = self.writeText "gdb-env-init" ''
+      directory ${debugFileStore}
+      set substitute-path /build ${debugFileStore}
+      # set debug-file-directory ${lib.concatMapStringsSep " " (p: "${p.debug}") debugPkgs}
+      set debug-file-directory ${debugFileStore}/lib/debug
+      '';
+    gdbWrapper = self.runCommand "gdb-for-${(builtins.head debugPkgs).name}" {} ''
+      mkdir -p $out/bin
+      cat >$out/bin/gdb <<EOF
+      #!/bin/sh
+      ${self.lib.getBin self.gdb}/bin/gdb -ix ${gdbInit} \$@
+      EOF
+      chmod +x $out/bin/gdb
+      '';
+    env = self.buildEnv {
+      name = "${gdbWrapper.name}-env";
+      paths =  debugPkgs ++ [ gdbWrapper ];
+      extraOutputsToInstall = [ "debug" ];
+    };
+  in env.overrideAttrs (oldAttrs: {passthru = {
+      debugPkgs = builtins.listToAttrs (map (p: { name = (builtins.parseDrvName p.name).name; value = p;}));
+      env = self.stdenv.mkDerivation {
+        name = "interactive-${gdbWrapper.name}-environment";
+        nativeBuildInputs = [ env ];
+
+        buildCommand = ''
+          echo >&2 ""
+          echo >&2 "*** gdbForPackage envs are meant for interactive use!***"
+          echo >&2 ""
+          exit 1
+        '';
+      };};}));
+  gdbForPackage = (pkg: self.gdbForPackages [pkg]);
 
   colmap-clang = self.libsForQt5.callPackage ./pkgs/colmap {
     stdenv = self.clangStdenv;
