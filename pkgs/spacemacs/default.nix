bc0fb46bdd15ae4caa03c3f9b01597fe700ca30b
timor <timor.dd@googlemail.com>
2019-04-01 16:23:08 +0200

    spacemacs: include outshine layer PR

--- pkgs/spacemacs/default.nix
+++ pkgs/spacemacs/default.nix
@@ -43,6 +43,12 @@ stdenv.mkDerivation rec {
       name = "factor-mode-patches";
     })
 
+    # preempt outshine/outorg layer PR
+    (fetchurl {
+      url = "https://patch-diff.githubusercontent.com/raw/syl20bnr/spacemacs/pull/11958.diff";
+      sha256 = "03whw92f5ds3jbgiia750h5h8dfqfggilllyiwgkv9pvd2d50k2j";
+    })
+
   ];
 
   postPatch = ''
