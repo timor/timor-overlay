479219f7302b0c80dbdd9f06e38fe4b9bbac9e00
timor <timor.dd@googlemail.com>
2020-01-31 14:07:32 +0100

    Revert "libdwarf: add zlib to buildInputs"
    This reverts commit 8f34306268af521f59dbd68926df33c0a5ac22d4.
    
    Present upstream, also this breaks `nix-env -qa`

--- default.nix
+++ default.nix
@@ -75,11 +75,6 @@ in
     patchShebangs $out/bin/git-rebase-all
     '';
 
-  inherit (callPackage (self.fetchurl {
-    url = https://raw.githubusercontent.com/timor/nixpkgs/libdwarf-dwarfdump-zlib/pkgs/development/libraries/libdwarf/default.nix;
-    sha256 = "043b55psin4wllz0vriwhm7p74w270312141zkk3radbnzfr38xf";
-  }) {}) libdwarf dwarfdump;
-
   spnav = callPackage ./pkgs/spnav { };
 
   spacenavd = callPackage ./pkgs/spacenavd { };
