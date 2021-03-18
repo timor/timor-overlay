bec688d4b612f9cbfabfc9f476a966de0692142c
timor <timor.dd@googlemail.com>
2021-03-18 10:28:52 +0100

    spacemacs: Try to restore correct value of GDK_PIXBUF_MDULE_FILE after start

--- pkgs/spacemacs/spacemacs-packages.nix
+++ pkgs/spacemacs/spacemacs-packages.nix
@@ -13,6 +13,10 @@ emacsPackages.overrideScope' (self: super: {
   emacs = super.emacs.overrideAttrs(oa: {
     patches = oa.patches ++ [./emacs-user-directory.patch ];
     buildInputs = oa.buildInputs ++ [ texinfo ];
+    preFixup = (oa.preFixup or "") + ''
+      gappsWrapperArgs=(--run export\ PARENT_GDK_PIXBUF_MODULE_FILE=\"\$GDK_PIXBUF_MODULE_FILE\" "''${gappsWrapperArgs[@]}")
+      gappsWrapperArgs+=(--add-flags --eval\ '"(setenv \"GDK_PIXBUF_MODULE_FILE\" (getenv \"PARENT_GDK_PIXBUF_MODULE_FILE\"))"')
+    '';
     name = "emacs-${oa.version}-spacemacs";
   });
 })
