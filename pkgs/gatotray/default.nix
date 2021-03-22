2cf2db41b03398829583cc5b99b94e9b6b3c2e35
timor <timor.dd@googlemail.com>
2021-03-22 13:35:13 +0100

    gatotray: incorporate upstream fix, add meta

--- pkgs/gatotray/default.nix
+++ pkgs/gatotray/default.nix
@@ -1,4 +1,4 @@
-{stdenv, pkgconfig, gdk_pixbuf, gtk2, fetchFromGitHub }:
+{stdenv, lib, pkgconfig, gdk_pixbuf, gtk2, fetchFromGitHub }:
 
 let version = "3.3"; in
 
@@ -13,15 +13,34 @@ stdenv.mkDerivation {
     sha256 = "08hy1zyq0nkrhmp599x6k0sn93a4484yi2lfgl0xf25lwdsrsyr5";
   };
 
-  patches = [ ./0001-index-on-fix-g-free-806f364-gatotray-3.3-with-memory.patch ];
-
   postPatch = ''
+    # Substitute hard-coded install destinations
     substituteInPlace Makefile --replace /usr/local/bin $out/bin --replace /usr/share/ $out/share/
+
+    # Leave stripping to fixup phase
     sed -i "/strip/d" Makefile
+
+    # fix #4 for latest stable version
+    patch -p1 <<EOF
+--- a/settings.c
++++ b/settings.c
+@@ -144,9 +144,9 @@
+     for(PrefString* s=pref_strings; s < pref_strings+G_N_ELEMENTS(pref_strings); s++)
+     {
+         GError* gerror = NULL;
+-        *s->value = s->default_value;
+         gchar* value = g_key_file_get_string(pref_file, "Options", s->description, &gerror);
+-        if(!gerror) *s->value = value;
++        if (!gerror) *s->value = value;
++        else *s->value = g_strdup(s->default_value);
+     }
+     preferences_changed();
+ }
+EOF
   '';
 
-  nativeBuildInputs = [pkgconfig];
-  buildInputs = [gdk_pixbuf gtk2];
+  nativeBuildInputs = [ pkgconfig ];
+  buildInputs = [ gdk_pixbuf gtk2 ];
 
   dontConfigure = true;
 
@@ -29,6 +48,13 @@ stdenv.mkDerivation {
     mkdir -p $out/bin
     mkdir -p $out/share/applications
     mkdir -p $out/share/icons
-
   '';
+
+  meta = {
+    description = "A minimalistic graphical system tray CPU monitor";
+    homepage = "https://github.com/gatopeich/gatotray";
+    license = lib.licenses.cc-by-30;
+    maintainers = [ lib.maintainers.timor ];
+    platforms = lib.platforms.linux;
+  };
 }
