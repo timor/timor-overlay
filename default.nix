d5e0a6b1902fd630ae81c810ed1bbed1c5b8b8f9
timor <timor.dd@googlemail.com>
2021-07-01 19:39:10 +0200

    notmuch: Include patch to display smime parts in notmuch-show

--- default.nix
+++ default.nix
@@ -165,6 +165,10 @@ in
 
   moore = callPackage ./pkgs/moore {};
 
+  notmuch = super.notmuch.overrideAttrs (oa: {
+    patches = (oa.patches or []) ++ [ ./patches/0001-Display-application-pkcs7-mime-parts-smime-decryptio.patch ];
+  });
+
   open-zwave = callPackage ./pkgs/open-zwave {};
 
   esp32 = callPackage ./pkgs/esp32 { };
