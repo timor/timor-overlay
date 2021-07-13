e665c78373f5e08e86740dcb0922fdb48a61367b
timor <timor.dd@googlemail.com>
2021-07-13 13:38:39 +0200

    patch gmime with x-pkcs7 fix

--- default.nix
+++ default.nix
@@ -153,6 +153,11 @@ in
 
   gatotray = callPackage ./pkgs/gatotray {};
 
+  gmime_patched = self.gmime3.overrideAttrs(oa: {
+    patches = (oa.patches or []) ++ [ ./patches/0001-Normalize-x-variants-for-comparing-protocol-and-mime.patch ];
+    doCheck = true;
+  });
+
   hachoir = callPackage ./pkgs/hachoir {};
 
   kerneldocs = callPackage ./pkgs/kerneldocs {};
@@ -167,9 +172,9 @@ in
 
   moore = callPackage ./pkgs/moore {};
 
-  notmuch = super.notmuch.overrideAttrs (oa: {
+  notmuch = (super.notmuch.overrideAttrs (oa: {
     patches = (oa.patches or []) ++ [ ./patches/0001-Display-application-pkcs7-mime-parts-smime-decryptio.patch ];
-  });
+  })).override{ gmime = self.gmime_patched; };
 
   open-zwave = callPackage ./pkgs/open-zwave {};
 
