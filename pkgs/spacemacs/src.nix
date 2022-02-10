aa33f794392df7a1c93e45693fd7e17e5acead8d
Michael Raitza <michael.raitza@tu-dresden.de>
2022-02-10 14:03:38 +0100

    spacemacs: Update sources and fix patch

--- pkgs/spacemacs/src.nix
+++ pkgs/spacemacs/src.nix
@@ -1,6 +1,6 @@
 {
   owner = "syl20bnr";
   repo = "spacemacs";
-  rev = "b992ad29bb0f0859db6df30bfac7492c49a8a22c";
-  sha256 = "12gh5rna6dalgyg1af1zcy26y638nmkns0lswbvv3rl5f5k08gvz";
+  rev = "efdfecbf8f72d3de4a410f9d653f8b91e569063c";
+  sha256 = "sha256-kLwTnGDqNHJxipqmv6qFe8zv+GK3P92vG9HTRhXxdJ0=";
 }
