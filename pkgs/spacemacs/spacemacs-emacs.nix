f02555a87458e599b42ff8edac53a5373e54b2fb
timor <timor.dd@googlemail.com>
2024-01-21 14:10:04 +0100

    spacemacs: don't build sqlite emacs package

--- pkgs/spacemacs/spacemacs-emacs.nix
+++ pkgs/spacemacs/spacemacs-emacs.nix
@@ -1,7 +1,7 @@
 {emacsPackages, extraPackages ? (p: [])}:
 emacsPackages.emacsWithPackages (epkgs: [
   # epkgs.ht                      #  bundled with spacemacs
-  epkgs.emacsql-sqlite
+  # epkgs.emacsql-sqlite        #  emacs-29 builtin
   epkgs.dash                    #  not bundled with spacemacs
   epkgs.s                       #  not bundled with spacemacs
   epkgs.f                       #  not bundled with spacemacs
