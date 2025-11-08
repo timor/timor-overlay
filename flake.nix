76ff3d1ec91ba3a3f97ef34beb305f0434296965
timor <timor.dd@googlemail.com>
2025-11-08 10:31:17 +0100

    try to fix flake voodoo

--- flake.nix
+++ flake.nix
@@ -1,14 +1,24 @@
 {
-  outputs = { self, nixpkgs, ... }: let
-    forAllSystems = f: nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system: f {
-      inherit system;
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ self.overlay ];
-      };
-    });
-  in {
-    overlay = final: prev: (import ./default.nix) final prev;
-    legacyPackages = forAllSystems ({ pkgs, ... }: pkgs);
+  inputs.nixpkgs = {
+    type = "indirect";
+    id = "nixpkgs";
   };
+
+  outputs = {nixpkgs, ...}: rec {
+    overlay = import ./default.nix;
+
+    legacyPackages."x86_64-linux" = import nixpkgs { system = "x86_64-linux"; overlays = [ overlay ]; config.allowUnfree = true; };
+  };
+  # outputs = { self, nixpkgs, ... }: let
+  #   forAllSystems = f: nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system: f {
+  #     inherit system;
+  #     pkgs = import nixpkgs {
+  #       inherit system;
+  #       overlays = [ self.overlay ];
+  #     };
+  #   });
+  # in {
+  #   overlay = final: prev: (import ./default.nix) final prev;
+  #   legacyPackages = forAllSystems ({ pkgs, ... }: pkgs);
+  # };
 }
